6f352b5e027ab5ada5fc63d4f806c1686de43239
Wendy <wendy.hj.wu@gmail.com>
2018-05-05 10:57:14 +0800

    set up admin authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  
+
   protect_from_forgery with: :exception
+
+  private
+    def authenticate_admin
+      unless current_user.admin?
+        flash[:alert] = "Not allow!"
+        redirect_to root_path
+      end
+    end
 end
