b1ba02ef4099f5654e6d99415c6b367a26ba8d2e
Wendy <wendy.hj.wu@gmail.com>
2018-06-03 10:17:06 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
   private
     def authenticate_admin
