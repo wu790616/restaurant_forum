a2ccf371467b03efa19518c289d9eb7e2fd32890
Wendy <wendy.hj.wu@gmail.com>
2018-06-09 09:25:12 +0800

    create comment model and setup relation between restaurant, user and comment

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,6 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name
   mount_uploader :image, PhotoUploader
   belongs_to :category
+  has_many :comments
   default_scope { order(created_at: :desc)}
 end
