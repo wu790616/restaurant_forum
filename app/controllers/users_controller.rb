3393bfe84d9235c2f827852d6336e6700156e450
Wendy <wendy.hj.wu@gmail.com>
2018-06-15 00:24:19 +0800

    add private method set_user in user controller and set it to before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
+
+  before_action :set_user
+
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
     redirect_to user_path(@user) unless @user == current_user
   end
 
   def update
-    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user)
   end
@@ -18,5 +18,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :intro, :avatar)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
