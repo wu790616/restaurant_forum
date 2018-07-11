50a745b844ed2e927dac2b03647f16f8a6b91f05
Wendy <wendy.hj.wu@gmail.com>
2018-07-11 11:21:50 +0800

    update favorites_count using counter_cache

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :user
-  belongs_to :restaurant
+  belongs_to :restaurant, counter_cache: true
+  # counter_cache 可以自動回傳 size 方法結果，計算關聯物件的數量
+  # 必須在關聯 Model 上加一個欄位，慣例命名為 {table_name}_count
 end
